187b6640a183b1cd4dbcd59171f69c2b71c0f5d3
shaannessy25 <shaan.hurley25@gmail.com>
2021-01-06 20:28:28 -0800

    adding navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-
+import CustomNavbar from './components/CustomNavbar'
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
+      <CustomNavbar />
       <h1>Hello World</h1>
       <p>Mission Statement</p>
     </div>
