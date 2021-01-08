7b36b8a0ff6d0e6d6571c0f921bd5f08e1cfdc9c
shaannessy25 <shaan.hurley25@gmail.com>
2021-01-07 18:26:34 -0800

    adding color to logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -4,16 +4,18 @@ import './Logo.css'
 
 function CustomLogo() {
     return (
-        <div className='container'>
+        <div className='container1'>
             <div className='logo'>
-                <div className="circle1"></div>
-                <div className='circle2'></div>
-                <div className='circle3'></div>
-                <div className='circle4'></div>
-                <div className='square1'></div>
-                <div className='square2'></div>
-                <div className='square3'></div>
-                <div className='square4'></div>
+                <div className='container5'>
+                    <div className="circle1"></div>
+                    <div className='circle2'></div>
+                    <div className='circle3'></div>
+                    <div className='circle4'></div>
+                    <div className='square1'></div>
+                    <div className='square2'></div>
+                    <div className='square3'></div>
+                    <div className='square4'></div>
+                </div>
             </div>
         </div>
     )
